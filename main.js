309d3c633008865098bdd0c32e24cc70250de37e
John Mullaney <j.a.mullaney1@gmail.com>
2024-07-11 06:53:19 -0700

    Fill data from each square into array using fill_data method

--- main.js
+++ main.js
@@ -12,25 +12,27 @@ function solve() {
     board.style.backgroundColor = "";
   }
 
-  fill_one();
+  fill_data();
 }
 
 document.querySelector(".solve").addEventListener("click", solve);
 
 document.addEventListener("DOMContentLoaded", main);
 
-let cell_one = [0, 0, 0, 0, 0, 0, 0, 0, 0]; // 0-2 first row, 3-5 second row, 6-8 last row
+let cells = [];
 
-let fill_one = () => {
-  // pulls data from problem square to fill data array for first cell
-  let i = 0; // need to loop through each index of the array even if there is no number there to keep the correct order
-  let data = document.getElementById("cellOne").getElementsByTagName("input"); // return an array of each input tag in the cellOne div
-  for (let input of data) {
-    if (input.value) {
-      // the empty string (no data in input field) is falsy! will only run the if statement if there is a number/something in the field. needs error handling but just first draft
-      cell_one[i] = parseInt(input.value); // add to cellone array in the correct location
+let fill_data = () => {
+  let data = document.querySelectorAll(".cell");
+  for (let cell of data) {
+    let square = [];
+    for (let input of cell.getElementsByTagName("input")) {
+      if (input.value) {
+        square.push(parseInt(input.value));
+      } else {
+        square.push(0);
+      }
     }
-    i++;
+    cells.push(square);
   }
-  console.log(cell_one);
+  console.log(cells);
 };
