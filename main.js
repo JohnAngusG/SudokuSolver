52aeb47649fa52a609b3a48ff96098e4b28640d4
John Mullaney <j.a.mullaney1@gmail.com>
2024-07-10 19:47:08 -0700

    Added comments for fill_one function

--- main.js
+++ main.js
@@ -19,16 +19,16 @@ document.querySelector(".solve").addEventListener("click", solve);
 
 document.addEventListener("DOMContentLoaded", main);
 
-document.getElementById("#userSquare");
-
-let cell_one = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+let cell_one = [0, 0, 0, 0, 0, 0, 0, 0, 0]; // 0-2 first row, 3-5 second row, 6-8 last row
 
 let fill_one = () => {
-  let i = 0;
-  let data = document.getElementById("cellOne").getElementsByTagName("input");
+  // pulls data from problem square to fill data array for first cell
+  let i = 0; // need to loop through each index of the array even if there is no number there to keep the correct order
+  let data = document.getElementById("cellOne").getElementsByTagName("input"); // return an array of each input tag in the cellOne div
   for (let input of data) {
     if (input.value) {
-      cell_one[i] = parseInt(input.value);
+      // the empty string (no data in input field) is falsy! will only run the if statement if there is a number/something in the field. needs error handling but just first draft
+      cell_one[i] = parseInt(input.value); // add to cellone array in the correct location
     }
     i++;
   }
