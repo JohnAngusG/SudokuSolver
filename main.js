da46708920f19adb5e8f99900b61bbf28c81b528
Tom Schuster <tomschuster02@gmail.com>
2024-07-11 00:32:30 -0700

    Get all cell data

--- main.js
+++ main.js
@@ -12,25 +12,35 @@ function solve() {
     board.style.backgroundColor = "";
   }
 
-  fill_one();
+  const cells = fillCells();
+
+  console.log(cells);
 }
 
 document.querySelector(".solve").addEventListener("click", solve);
 
 document.addEventListener("DOMContentLoaded", main);
 
-let cell_one = [0, 0, 0, 0, 0, 0, 0, 0, 0]; // 0-2 first row, 3-5 second row, 6-8 last row
+let fillCells = () => {
+  let cellArray = [];
 
-let fill_one = () => {
-  // pulls data from problem square to fill data array for first cell
-  let i = 0; // need to loop through each index of the array even if there is no number there to keep the correct order
-  let data = document.getElementById("cellOne").getElementsByTagName("input"); // return an array of each input tag in the cellOne div
-  for (let input of data) {
-    if (input.value) {
-      // the empty string (no data in input field) is falsy! will only run the if statement if there is a number/something in the field. needs error handling but just first draft
-      cell_one[i] = parseInt(input.value); // add to cellone array in the correct location
+  let i = 0;
+  let cells = document.getElementsByClassName("cell");
+
+  for (let cell of cells) {
+    let inputs = cell.getElementsByTagName("input");
+
+    let singleCell = Array(9).fill(0);
+
+    for (let j = 0; j < inputs.length; j++) {
+      if (inputs[j].value) {
+        singleCell[j] = parseInt(inputs[j].value);
+      }
     }
+
+    cellArray.push(singleCell);
     i++;
   }
-  console.log(cell_one);
+
+  return cellArray;
 };
