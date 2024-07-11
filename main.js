aee33abc5174322c0048928b2320abae596e3082
John Mullaney <j.a.mullaney1@gmail.com>
2024-07-10 19:42:51 -0700

    Correct html syntax/spacing and create first algo for parsing numbers to cell array

--- main.js
+++ main.js
@@ -11,8 +11,26 @@ function solve() {
   } else {
     board.style.backgroundColor = "";
   }
+
+  fill_one();
 }
 
 document.querySelector(".solve").addEventListener("click", solve);
 
 document.addEventListener("DOMContentLoaded", main);
+
+document.getElementById("#userSquare");
+
+let cell_one = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+let fill_one = () => {
+  let i = 0;
+  let data = document.getElementById("cellOne").getElementsByTagName("input");
+  for (let input of data) {
+    if (input.value) {
+      cell_one[i] = parseInt(input.value);
+    }
+    i++;
+  }
+  console.log(cell_one);
+};
