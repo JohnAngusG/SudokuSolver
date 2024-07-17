7f338a92d8d5fc511d04ed8e2b53e82a16eadd11
Tom Schuster <tomschuster02@gmail.com>
2024-07-16 23:30:10 -0700

    prep

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 // Script: main.js
 function main() {
-  console.log("Welcome");
+  console.log('Welcome');
 }
 
 function solve() {
-  const board = document.querySelector("#userSquare");
+  const board = document.querySelector('#userSquare');
 
   if (!board.style.backgroundColor) {
-    board.style.backgroundColor = "lightgrey";
+    board.style.backgroundColor = 'lightgrey';
   } else {
-    board.style.backgroundColor = "";
+    board.style.backgroundColor = '';
   }
 
   const cells = fillCells();
@@ -17,30 +17,6 @@ function solve() {
   console.log(cells);
 }
 
-document.querySelector(".solve").addEventListener("click", solve);
+document.querySelector('.solve').addEventListener('click', solve);
 
-document.addEventListener("DOMContentLoaded", main);
-
-let fillCells = () => {
-  let cellArray = [];
-
-  let i = 0;
-  let cells = document.getElementsByClassName("cell");
-
-  for (let cell of cells) {
-    let inputs = cell.getElementsByTagName("input");
-
-    let singleCell = Array(9).fill(0);
-
-    for (let j = 0; j < inputs.length; j++) {
-      if (inputs[j].value) {
-        singleCell[j] = parseInt(inputs[j].value);
-      }
-    }
-
-    cellArray.push(singleCell);
-    i++;
-  }
-
-  return cellArray;
-};
+document.addEventListener('DOMContentLoaded', main);
