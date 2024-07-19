a0b7e733adf635cbb7e73a427110397269ba04fd
John Mullaney <j.a.mullaney1@gmail.com>
2024-07-18 22:02:17 -0700

    Finalized checkRow helper function, needed to check included but use opposite result. Solved sudoku square with one cell missing

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function solve() {
 
   fill_data();
   generalFunc();
-  console.log(sudoku);
+  console.log(testSudoku);
 }
 
 document.querySelector(".solve").addEventListener("click", solve);
@@ -36,16 +36,14 @@ let fill_data = () => {
     }
     sudoku.push(row);
   }
-  console.log(sudoku);
 };
 
 let checkRow = (row, testNumber) => {
-  return row.includes(testNumber);
+  return !row.includes(testNumber);
 };
 
 let checkColumn = (columnNumber, testNumber) => {
   for (let row of sudoku) {
-    console.log(row[columnNumber]);
     if (row[columnNumber] === testNumber) {
       return false;
     }
@@ -54,20 +52,34 @@ let checkColumn = (columnNumber, testNumber) => {
 };
 
 let generalFunc = () => {
-  for (let row of sudoku) {
+  for (let row of testSudoku) {
     for (let cell of row) {
-      if (!cell) {
+      if (cell == 0) {
         let indexOfCell = row.indexOf(cell);
         let options = [];
         for (let i = 1; i < 10; i++) {
+          console.log(i);
           if (checkRow(row, i) && checkColumn(indexOfCell, i)) {
             options.push(i);
           }
         }
+        console.log(options);
         if (options.length === 1) {
-          row[indexOfCell] = options.pop();
+          row[indexOfCell] = options[0];
         }
       }
     }
   }
 };
+
+let testSudoku = [
+  [7, 4, 2, 6, 1, 8, 9, 3, 5],
+  [1, 8, 3, 2, 9, 5, 6, 4, 7],
+  [6, 5, 9, 3, 7, 4, 1, 2, 8],
+  [4, 7, 8, 5, 3, 6, 2, 1, 9],
+  [9, 3, 1, 4, 2, 7, 5, 8, 6],
+  [2, 6, 5, 9, 8, 1, 3, 7, 4],
+  [3, 9, 6, 8, 4, 0, 7, 5, 1],
+  [8, 2, 7, 1, 5, 9, 4, 6, 3],
+  [5, 1, 4, 7, 6, 3, 8, 9, 2],
+];
